48b796ddde2f4ac036cce56b340dc68b54a1dee0
TheAlienDoctor <TheAlienDoctorYT@gmail.com>
2023-01-13 10:08:56 +0000

    Removed pack combiner (it will return)

--- js/resource-pack-combiner/collapsible.js
+++
@@ -1,4 +0,0 @@
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function(collapseEl) {
-    return new bootstrap.Collapse(collapseEl)
-})
