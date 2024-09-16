fb9ac238c68f47e8e7dcb98ae7233f45e90c273a
Niceygy <niceygy@niceygylive.xyz>
2024-09-16 11:22:05 +0100

    Thank goodness for find & replace (fixed download buttons)

--- js/WDR.js
+++ js/WDR.js
@@ -1,6 +1,6 @@
 /*
 WDR.JS Worker Download Recorder client for TheAlienDoctor.com
-By Niceygy (Ava Whale). For help, please see github.com/ufo-studios
+By Niceygy. For help, please see github.com/ufo-studios
 WARNING! This script will NOT use any cache. (Cache bypasses it). 
 IF you see high/abnormal traffic, check this!
 */
@@ -8,12 +8,39 @@ IF you see high/abnormal traffic, check this!
 const url = new URL(document.referrer);
 const path = url.pathname.substring(1); // Remove the first '/'
 
-
-fetch('https://download.thealiendoctor.com?page='+path, { cache: 'no-cache' })
-  .then(response => response.text())
-  .then(data => {
+fetch("https://download.thealiendoctor.com?page=" + path, { cache: "no-cache" })
+  .then((response) => response.text())
+  .then((data) => {
     // console.log(data);
   })
-  .catch(error => {
-    console.error('Error making the request:', error);
+  .catch((error) => {
+    console.error("Error making the request:", error);
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const downloadButtons = document.querySelectorAll(".btn-blue");
+
+  downloadButtons.forEach((button) => {
+    button.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the default action (navigation)
+
+      console.log(this.dataset.redirectUrl);
+      console.log(this);
+
+      const url = new URL(this.dataset.redirectUrl);
+
+      // Optionally, log the modified URL
+      fetch(this.dataset.redirectUrl, { cache: "no-cache" })
+        .then((response) => response.text())
+        .then((data) => {
+          // console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error making the request:", error);
+        });
+
+      // Navigate to the modified URL
+      window.location.href = url.toString();
+    });
+  });
+});
